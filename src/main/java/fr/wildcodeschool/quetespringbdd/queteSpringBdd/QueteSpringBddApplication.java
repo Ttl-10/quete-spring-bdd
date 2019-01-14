4f19943556f21a1b7375cbe4a086148e51d99ddd
Tantely Randria <tantely.tnr@gmail.com>
2019-01-15 00:05:02 +0100

    First commit

---
+++ src/main/java/fr/wildcodeschool/quetespringbdd/queteSpringBdd/QueteSpringBddApplication.java
@@ -0,0 +1,14 @@
+package fr.wildcodeschool.quetespringbdd.queteSpringBdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteSpringBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteSpringBddApplication.class, args);
+	}
+
+}
+
